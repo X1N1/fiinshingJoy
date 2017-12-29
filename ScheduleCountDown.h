dcce04c3cf1aee98ab37b42eec3ac9576e790329
hamunan <32032159+hamunan@users.noreply.github.com>
2017-12-29 21:45:26 +0800

    Add files via upload

---
+++ ScheduleCountDown.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "cocos2d.h"
+#include "ScheduleCounterDelegate.h"
+
+USING_NS_CC;
+
+class ScheduleCountDown :
+	public CCNode
+{
+public:
+	//倒计时初始设置 
+	static ScheduleCountDown* create(ScheduleCounterDelegate* target, int perimeter = 99, bool loop = true);
+
+	bool init (ScheduleCounterDelegate* target, int perimeter = 99, bool loop = true);
+
+	CC_SYNTHESIZE(bool, _loop, Loop);
+
+protected:
+	ScheduleCounterDelegate* _target;
+	int _currTime;
+	int _maxTime;
+	void schedulePerSecond(float delta);
+};
+
